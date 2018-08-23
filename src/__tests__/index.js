a187f67b3d3182bf6b0b0fbc1faf44a21bb05a04
Leath Cooper <leathcooper@gmail.com>
2018-08-23 00:14:35 -0400

    add a test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -5,30 +5,60 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import App from 'fusion-core';
-import {getService} from 'fusion-test-utils';
-
+import {createPlugin} from 'fusion-core';
+import App from 'fusion-react';
+import {getSimulator} from 'fusion-test-utils';
+import Redux, {
+  ReduxToken,
+  EnhancerToken,
+  ReducerToken,
+} from 'fusion-plugin-react-redux';
 import Plugin from '../index.js';
 
 Enzyme.configure({adapter: new Adapter()});
 
 /* Test fixtures */
-const appCreator = () => {
-  const app = new App('test', el => el);
-  return () => app;
+const appCreator = Component => {
+  const app = new App(<Component />, el => el);
+  app.register(ReduxToken, Redux);
+  app.register(ReducerToken, (state = {count: 0, test: false}, action) => {
+    switch (action.type) {
+      case 'GOOD_ACTION': {
+        return {
+          ...state,
+          test: true,
+          count: state.count + 1,
+        };
+      }
+      default: {
+        return state;
+      }
+    }
+  });
+  app.register(EnhancerToken, Plugin);
+  return app;
 };
 
-tape('browser middleware', async t => {
-  const element = React.createElement('div');
-  const ctx: any = {element, template: {body: []}, memoized: new Map()};
-  const service = getService(appCreator(), Plugin);
-  try {
-    await (Plugin.middleware &&
-      // $FlowFixMe
-      Plugin.middleware(null, service)((ctx: any), () => Promise.resolve()));
-  } catch (e) {
-    t.ifError(e);
-  }
-
+tape('store enhancer', async t => {
+  t.plan(4);
+  const Root = () => <div />;
+  const app = appCreator(Root);
+  app.register(
+    createPlugin({
+      deps: {redux: ReduxToken},
+      middleware: ({redux}) => async (ctx, next) => {
+        await next();
+        const store = redux.from(ctx).store;
+        t.equal(store.getState().test, false, 'reducer is correct');
+        t.equal(store.getState().count, 0, 'reducer is correct');
+        // dispatch a thunk, not sure how to test this stuff
+        store.dispatch(dispatch => dispatch({type: 'GOOD_ACTION'}));
+        t.equal(store.getState().test, true, 'reducer is correct');
+        t.equal(store.getState().count, 1, 'reducer is correct');
+      },
+    })
+  );
+  const sim = getSimulator(app);
+  await sim.render('/');
   t.end();
 });
